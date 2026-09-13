8334e6c7400554b6b52ffd9136d68bc345db3526
Niharika Rathor <74825076+thisNiharika@users.noreply.github.com>
2026-09-13 21:55:28 +0530

    add project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,27 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter, withViewTransitions, withInMemoryScrolling } from '@angular/router';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners
+} from '@angular/core';
+
+import {
+  provideRouter,
+  withViewTransitions,
+  withInMemoryScrolling
+} from '@angular/router';
+
+import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes, withViewTransitions())
+
+    provideRouter(
+      routes,
+      withViewTransitions()
+    ),
+
+    provideHttpClient()
   ]
 };
